984044a12367a328d97ad403835130ead17d6ffa
Tomasz <tomasz@MacBook-Pro-Tomasz.local>
2022-11-08 09:43:10 +0100

    fix createCart missing channel_id

--- config/constants.js
+++ config/constants.js
@@ -2,3 +2,5 @@ export const API_URL =
   process.env.DEPLOY_PLATFORM === 'netlify'
     ? process.env.NETLIFY_API_URL || 'http://localhost:8888/.netlify/functions'
     : '';
+
+export const CHANNEL_ID = process.env.CHANNEL_ID
